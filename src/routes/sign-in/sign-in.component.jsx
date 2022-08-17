c9f817277c59b8993a76d183706e915b2adb819b
Todd Eric Whitehead <warriorfullarmorofgod@gmail.com>
2022-08-17 11:42:20 -0700

    96 - Sign In With React 9:26 - go first part to work!

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,22 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from 'firebase/auth';
 
-import { createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { 
+    auth,
+    signInWithGooglePopup,
+    signInWithGoogleRedirect,
+    createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { async } from "@firebase/util";
 
 const SignIn = () => {
+    useEffect(() => {
+        async function fetchData() {
+            const response = await getRedirectResult(auth);
+            console.log(response);
+        }
+        fetchData();
+    }, []);
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         console.log(user);
@@ -15,6 +29,9 @@ const SignIn = () => {
             <h1>Sign IN Page</h1>
             <button onClick={logGoogleUser}>
                 Sign In with Google Popup
+            </button>
+            <button onClick={signInWithGoogleRedirect}>
+                Sign In with Google Redirect
             </button>
         </div>
     );
