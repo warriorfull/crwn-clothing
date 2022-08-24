559183353144d87e84be812722bef75e74a5a042
Todd Eric Whitehead <warriorfullarmorofgod@gmail.com>
2022-08-24 11:48:48 -0700

    101 - Custom Button Component - part 2

--- src/components/sign-up/sign-up-form.component.jsx
+++ src/components/sign-up/sign-up-form.component.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 import './sign-up-form.styles.scss';
 
@@ -91,7 +93,7 @@ const SignUpForm = () => {
                     name="confirmPassword" 
                     value={confirmPassword} 
                 />
-                <button type="submit">Sign Up</button>
+                <Button type="submit">Sign Up</Button>
             </form>
         </div>
     )
