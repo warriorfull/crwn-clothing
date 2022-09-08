0278523c87f22b9421c9c17bf905a63a8735255b
Todd Eric Whitehead <warriorfullarmorofgod@gmail.com>
2022-09-08 13:52:02 -0700

    111 - Observer Pattern

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -29,9 +29,7 @@ const SignInForm = () => {
     };
 
     const signInWithGoogle = async () => {
-        const { user } = await signInWithGooglePopup();
-        // setCurrentUser(user);
-        await createUserDocumentFromAuth(user);
+        await signInWithGooglePopup();
     }
 
     const handleSubmit = async (event) => {
