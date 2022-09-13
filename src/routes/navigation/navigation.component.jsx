210e637214d809c2d9270623761346e017213445
Todd Eric Whitehead <warriorfullarmorofgod@gmail.com>
2022-09-13 09:55:18 -0700

    115 - Cart Icon and Dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,9 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.contexts';
 
@@ -31,8 +34,10 @@ const Navigation = () => {
                         </Link>
                     )
                 }
+                <CartIcon />
                 
             </div>
+            <CartDropdown />
         </div>
         <Outlet />
       </Fragment>
